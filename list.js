54d5cb6fdaa1b5fd959e58c66ff5953c735d5322
Simon Crabtree <simon@crabsticks.net>
2016-01-15 19:39:40 +0000

    List now shows more info about each blob

--- list.js
+++ list.js
@@ -3,6 +3,10 @@ var azure = require('azure-storage')
 var blobSvc = azure.createBlobService();
 
 blobSvc.listBlobsSegmented('testcontainer', null, function(err, result,response){
-	if(!err)
-		console.log(result)
+	if(err)
+		return;
+
+	result.entries.forEach(function(blob) {
+		console.log(blob)
+	})
 })
